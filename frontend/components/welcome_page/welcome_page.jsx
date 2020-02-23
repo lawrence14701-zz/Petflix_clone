55b1920047cf3de33bf601a302f53ad133ca433f
lawrence_nic <your_email_here>
2020-02-23 01:10:39 -0500

    added js to the buttons

--- frontend/components/welcome_page/welcome_page.jsx
+++ frontend/components/welcome_page/welcome_page.jsx
@@ -1,220 +1,237 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-
+import React from "react";
 
 class WelcomePage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            tabItem: false,
-        }
-        // this.tabItem = this.tabItem.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchOn: [true, false, false]
+    };
+    this.switchOnOff = this.switchOnOff.bind(this);
+  }
 
-    // tabItem(e){
-    //     debugger
-    //    const currentState = this.state.tabItem;
-    //     // this.setState({tabItem: !currentState})
-    // }
+  switchOnOff(index) {
+    let switches = [false, false, false];
+    return () => {
+      switches[index] = true;
+      this.setState({ switchOn: switches });
+    };
+  }
 
+  render() {
+    return (
+      <>
+        <header className="showcase">
+          <div className="showcase-top">
+              <img src={window.logo} alt="logo"/>
+            <a href="" className="btn btn-rounded">
+              Sign In
+            </a>
+          </div>
+          <div className="showcase-content">
+            <h1>See what's next</h1>
+            <p>Watch anywhere. Cancel anytime</p>
+            <a href="" className="btn btn-xl">
+              watch free for 30 days{" "}
+              <i className="fas fa-chevron-right btn-icon"></i>
+            </a>
+          </div>
+        </header>
 
-    render(){
-        return (
-          <>
-            <header className="showcase">
-              <div className="showcase-top">
-                {/* $$$TODO add logo */}
-                <h1>Petflix</h1>
-                <a href="" className="btn btn-rounded">
-                  Sign In
+        <section className="tabs">
+          <div className="container">
+            <div
+              id="tab-1"
+              className={
+                this.state.switchOn[0] ? "tab-item tab-border" : "tab-item"
+              }
+              onClick={this.switchOnOff(0)}
+            >
+              <i className="fas fa-door-open fa-3x"></i>
+              <p className="hide-sm">Cancel Anytime</p>
+            </div>
+            <div
+              id="tab-2"
+              className={this.state.switchOn[1] ? "tab-item tab-border" : "tab-item"}
+              onClick={this.switchOnOff(1)}
+            >
+              <i className="fas fa-tablet-alt fa-3x"></i>
+              <p className="hide-sm">Watch Anywhere</p>
+            </div>
+            <div
+              id="tab-3"
+              className={this.state.switchOn[2] ? "tab-item tab-border" : "tab-item"}
+              onClick={this.switchOnOff(2)}
+            >
+              <i className="fas fa-tags fa-3x"></i>
+              <p className="hide-sm">Pick your price</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="tab-content">
+          {/* TAB ONE CONTENT */}
+          <div className="container">
+            <div
+              id="tab-1-content"
+              className={this.state.switchOn[0] ? "tab-item" : "hide-tab-item"}
+            >
+              <div className="tab-1-content-inner">
+                <div>
+                  <p className="text-lg text-center">Watch everywhere.</p>
+                  <p className="text-center">
+                    Stream unlimited pet movies and pet TV shows on your phone, tablet,
+                    laptop, and TV without paying more.
+                  </p>
+                </div>
+                <img src={window.tabContentOne} alt="content-1" />
+              </div>
+            </div>
+
+            {/* TAB TWO CONTENT */}
+            <div
+              id="tab-2-content"
+              className={this.state.switchOn[1] ? "tab-item" : "hide-tab-item"}
+            >
+              <div className="tab-2-content-inner">
+                <div>
+                  <p className="text-lg text-center">Browse the latest pet films.</p>
+                  <p className="text-center">
+                    Search through endless amounts of pet films until you find the right one.
+                  </p>
+                </div>
+                <img src={window.tabContentTwo} alt="content-1" />
+              </div>
+            </div>
+
+            {/* TAB 3 CONTENT */}
+            <div
+              id="tab-3-content"
+              className={this.state.switchOn[2] ? "tab-item" : "hide-tab-item"}
+            >
+              <div className="text-center">
+                <p className="text-lg">
+                  Choose one plan and watch everything on Netflix.
+                </p>
+                <a href="#" className="btn btn-lg">
+                  Watch Free For 30 Days
                 </a>
               </div>
-              <div className="showcase-content">
-                <h1>See what's next</h1>
-                <p>watch anywhere. Cancel anytime</p>
-                <a href="" className="btn btn-xl">
-                  watch free for 30 days{" "}
-                  <i className="fas fa-chevron-right btn-icon"></i>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Basic</th>
+                    <th>Standard</th>
+                    <th>Premium</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Monthly price after free month ends</td>
+                    <td>$8.99</td>
+                    <td>$12.99</td>
+                    <td>$15.99</td>
+                  </tr>
+                  <tr>
+                    <td>HD Available</td>
+                    <td>
+                      <i className="fas fa-times"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Ultra HD Available</td>
+                    <td>
+                      <i className="fas fa-times"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-times"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Screens you can watch on at the same time</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td>Watch on your laptop, TV, phone and tablet</td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Unlimited movies and TV shows</td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Cancel anytime</td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>First month free</td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                    <td>
+                      <i className="fas fa-check"></i>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </section>
+
+        <footer className="footer">
+          <p>Designed by Lawrence Nicastro</p>
+          <div className="footer-cols">
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/lawrencenicastro">
+                  <span>LinkedIn </span>
+                  <i className="fab fa-linkedin"></i>
                 </a>
-              </div>
-            </header>
-
-            <section className="tabs">
-              <div className="container">
-                <div id="tab-1" className="tab-item tab-border">
-                  <i className="fas fa-door-open fa-3x"></i>
-                  <p className="hide-sm">Cancel Anytime</p>
-                </div>
-                <div id="tab-2" className="tab-item"
-                >
-                  <i className="fas fa-tablet-alt fa-3x"></i>
-                  <p className="hide-sm">Watch Anywhere</p>
-                </div>
-                <div id="tab-3" className="tab-item">
-                  <i className="fas fa-tags fa-3x"></i>
-                  <p className="hide-sm">Pick your price</p>
-                </div>
-              </div>
-            </section>
-
-            <section className="tab-content">
-              {/* TAB ONE CONTENT */}
-              <div className="container">
-                <div id="tab-1-content" className="tab-content-item show">
-                  <div className="tab-2-content-inner">
-                    <div>
-                      <p className="text-lg text-center">Watch everywhere.</p>
-                      <p className="text-center">
-                        Stream unlimited movies and TV shows on your phone,
-                        tablet, laptop, and TV without paying more.
-                      </p>
-                    </div>
-                    <img src={window.tabContentOne} alt="content-1" />
-                  </div>
-                </div>
-
-                {/* TAB TWO CONTENT */}
-                <div id="tab-2-content" className="tab-content-item show">
-                  <div className="tab-2-content-inner">
-                    <div>
-                      <p className="text-lg text-center">Watch everywhere.</p>
-                      <p className="text-center">
-                        Stream unlimited movies and TV shows on your phone,
-                        tablet, laptop, and TV without paying more.
-                      </p>
-                    </div>
-                    <img src={window.tabContentOne} alt="content-1" />
-                  </div>
-                </div>
-
-                {/* TAB 3 CONTENT */}
-                <div id="tab-3-content" className="tab-content-item">
-                  <div className="text-center">
-                    <p className="text-lg">
-                      Choose one plan and watch everything on Netflix.
-                    </p>
-                    <a href="#" className="btn btn-lg">
-                      Watch Free For 30 Days
-                    </a>
-                  </div>
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th></th>
-                        <th>Basic</th>
-                        <th>Standard</th>
-                        <th>Premium</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Monthly price after free month ends on 6/19/19</td>
-                        <td>$8.99</td>
-                        <td>$12.99</td>
-                        <td>$15.99</td>
-                      </tr>
-                      <tr>
-                        <td>HD Available</td>
-                        <td>
-                          <i className="fas fa-times"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Ultra HD Available</td>
-                        <td>
-                          <i className="fas fa-times"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-times"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Screens you can watch on at the same time</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>4</td>
-                      </tr>
-                      <tr>
-                        <td>Watch on your laptop, TV, phone and tablet</td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Unlimited movies and TV shows</td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Cancel anytime</td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>First month free</td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                        <td>
-                          <i className="fas fa-check"></i>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </section>
-
-            <footer className="footer">
-              <p>Questions call 347-617-2607</p>
-              <div className="footer-cols">
-                <ul>
-                  <li>
-                    <a href="https://www.linkedin.com/in/lawrencenicastro">
-                      <span>LinkedIn </span>
-                      <i className="fab fa-linkedin"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </footer>
-          </>
-        );
-    }
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </>
+    );
+  }
 }
 
 export default WelcomePage;
