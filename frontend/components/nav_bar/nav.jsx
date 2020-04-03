2999a49539124a3ccf63b5f712163c2b439dfdae
lawrence_nic <lawrence.nicastro1@gmail.com>
2020-04-02 20:20:49 -0400

    update nav

--- frontend/components/nav_bar/nav.jsx
+++ frontend/components/nav_bar/nav.jsx
@@ -11,16 +11,15 @@ class Nav extends React.Component {
   render() {
     return (
       <header>
-      <div class="netflixLogo">
+      <div class="petflixLogo">
         <a id="logo" href=""><img src={window.logo} alt="Logo Image"/></a>
       </div>      
       <nav class="main-nav">                
         <a href="#home">Home</a>
-        <a href="#tvShows">TV Shows</a>
+        <a href="#tvShows">Genres</a>
         <a href="#movies">Movies</a>
-        <a href="#originals">Originals</a>
-        <a href="#">Recently Added</a>
-        <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">Portfolio</a>        
+        <a href="#">My List</a>
+        <a target="_blank" href="">Portfolio</a>        
       </nav>
       <nav class="sub-nav">
         <a href="#"><i class="fas fa-search sub-nav-logo"></i></a>
