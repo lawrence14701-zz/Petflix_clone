ba6bac210749015dff242aa3f46ad7be8c0dec34
lawrence_nic <lawrence.nicastro1@gmail.com>
2020-04-06 01:56:45 -0400

    add custom play bar for video

--- frontend/components/home/homePage.jsx
+++ frontend/components/home/homePage.jsx
@@ -11,8 +11,27 @@ class HomePage extends React.Component {
 
   render() {
     const { genres, movies } = this.props;
-    const randomMovie = Object.values(movies)[Math.floor(Math.random())];
-    const advertisement = randomMovie === undefined ? null : <video src={randomMovie.video}></video>;
+    const randomMovie = Object.values(movies)[Math.floor(Math.random())]; //why is it always on the same movie no matter what, shouldn't it be random
+
+    const advertisement =
+      randomMovie === undefined ? null : (
+        <div className="vid-container">
+          <video className="advertise-video" src={randomMovie.video}></video>
+          <div className="ad-info">
+            <div id="ad-title">{randomMovie.title}</div>
+            <div id="ad-description">{randomMovie.description}</div>
+            <div id="ad-buttons"> why is this not showing??!?!?!?!?!?!?!??!!?!
+              {/* <button id="ad-play">
+                <i className="fas fa-play"></i>Play
+              </button>
+              <button id="ad-list">
+                <i className="fas fa-plus-circle"></i>Add to my list
+              </button> */}
+            </div>
+          </div>
+        </div>
+      );
+
     const sliders = genres.map((genre) => {
       //find the movies that belong to a specific genre
       let movieCategory = [];
--- frontend/components/play_movie/playMovie.jsx
+++ frontend/components/play_movie/playMovie.jsx
@@ -3,6 +3,20 @@ import React from "react";
 class PlayMovie extends React.Component {
   constructor(props) {
     super(props);
+    this.handleMoviePlayer = this.playMovie.bind(this)
+
+    this.state = {
+      playing: false,
+    }
+  }
+
+  playMovie(){
+    const { playing } = this.state
+    const moviePlayer = this.refs.moviePlayer
+    const currentState = this.state.playing;
+    this.setState({playing: !currentState});
+
+    playing ? moviePlayer.pause() : moviePlayer.play();
   }
 
   componentDidMount() {
@@ -11,19 +25,23 @@ class PlayMovie extends React.Component {
 
   render() {
     const movie = this.props.movie;
+    const {playing} = this.state;
     if (movie) {
       return (
-        <>
-          <video
-            className="full-screen-movie"
-            src={movie.video}
-            width="80"
-            height="320"
-            onMouseOut={this.pauseVideo}
-            onMouseEnter={this.playVideo}
-            controls
-          />
-        </>
+        <div className="play-container">
+          <div id="play-video">
+            <video className="full-screen-movie" src={movie.video} ref='moviePlayer'/>
+            <div className="controls">
+              <div className="bar">
+                <div className="bar-color"></div>
+              </div>
+              <div className="buttons">
+                <button id={playing ? 'pause-movie':'play-movie'} onClick={this.handleMoviePlayer}>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       );
     } else {
       return null;
