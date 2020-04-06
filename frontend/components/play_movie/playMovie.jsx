bcbe5a41117a2b241965efcb4155ffd80626fedd
lawrence_nic <lawrence.nicastro1@gmail.com>
2020-04-06 02:52:24 -0400

    nav styling

--- frontend/components/play_movie/playMovie.jsx
+++ frontend/components/play_movie/playMovie.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class PlayMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.handleMoviePlayer = this.playMovie.bind(this)
+    this.handleMoviePlayer = this.togglePlayPause.bind(this)
 
     this.state = {
       playing: false,
     }
   }
 
-  playMovie(){
+  togglePlayPause(){
     const { playing } = this.state
     const moviePlayer = this.refs.moviePlayer
     const currentState = this.state.playing;
@@ -21,6 +22,16 @@ class PlayMovie extends React.Component {
 
   componentDidMount() {
     this.props.fetchMovie(this.props.match.params.movieId);
+    // const moviePlayer = this.refs.moviePlayer;
+    // debugger
+    // const bar = this.refs.bar;
+    // moviePlayer.addEventListener('timeupdate', function(){
+    //   barPosition = moviePlayer.currentTime / moviePlayer.duration;
+    //   $(bar).css({
+    //     'width': '20%'
+    //   })
+    //   if(moviePlayer.ended) this.setState({playing:false})
+    // })
   }
 
   render() {
@@ -30,10 +41,11 @@ class PlayMovie extends React.Component {
       return (
         <div className="play-container">
           <div id="play-video">
+            <div id='back'><Link to='/browse'><i className="fas fa-arrow-left"></i></Link></div>
             <video className="full-screen-movie" src={movie.video} ref='moviePlayer'/>
             <div className="controls">
               <div className="bar">
-                <div className="bar-color"></div>
+                <div className="bar-color" ref='bar'></div>
               </div>
               <div className="buttons">
                 <button id={playing ? 'pause-movie':'play-movie'} onClick={this.handleMoviePlayer}>
